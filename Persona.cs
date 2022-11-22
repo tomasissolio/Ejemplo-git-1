06ec7cfccb4ef125d8f6385dcb62f545c4787b8c
Tomas <tomasissolio@gmail.com>
2022-11-22 12:33:07 -0300

    Primer commit del ejemplo

---
+++ Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_git
+{
+    class Persona
+    {
+        public string nombre { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+            persona1.nombre = "Juan";
+
+            Console.WriteLine($"Nombre de la persona: {persona1.nombre}");
+        }
+    }
+}
